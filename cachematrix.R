6846eb456e5af04a6131eb9261af37b8b96c35b2
Sceadu <benchen@live.com.au>
2017-04-09 21:00:58 +1000

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2: Lexical Scoping
+## Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
-
+## makeCacheMatrix greates four functions, set(), get(), setinv() and getinv()
+## and returns the functions within a list to the parent environment
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL #sets 'i' to NULL
+  set <- function(y) {
+    x <<- y #sets the input argument to 'x' in the parent environment
+    i <<- NULL #sets 'i' to Null in the parent environment
+  }
+  get <- function() x #gets the matrix assigned to 'x'
+  setinv <- function(inv) i <<- inv #sets the inverse of the matrix to 'i' 
+  getinv <- function() i #gets the inverse of the matrix 'i' 
+  list(set = set,  # name the set() function as 'set'
+       get = get,  # name the get() function as 'get' 
+       setinv = setinv, # name the setinv() as 'setinv' 
+       getinv = getinv) # name the getinv() as 'getinv'
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve retrieves or calculates and stores the inverse of the matrix 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv() #retrieves the cached inverse of the matrix and sets to 'i'
+  if(!is.null(i)) { #if 'i' is not NULL
+    message("getting cached data") #prints the message
+    return(i) #returns 'i'
+  }
+  #if 'i' is NULL
+  data <- x$get() #gets 'x' from the parent environment and set 'data' to 'x'
+  i <- solve(data, ...) #generates the inverse of matrix 'data' and set to 'i'
+  x$setinv(i) #stores 'i', the inverse of matrix 'x'
+  i ## Returns 'i', a matrix that is the inverse of 'x'
 }
